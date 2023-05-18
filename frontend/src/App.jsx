ce43b83709c467708b95c3c10882bf77241788ea
kevinpan47 <kpan8055@gmail.com>
2023-05-18 18:53:06 +0000

    removed joyui, mui, threejs; added tailwind, firebase config; added README

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 // import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Navbar from "./components/Navbar";
 import Home from "./components/Home";
