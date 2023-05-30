1c7d5e99d2a19c1f1447428407efa582a91b3e5c
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-05-30 14:14:46 -0500

    added logo, About, css for Home

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <>
+      <h2>About Us</h2>
+      <div className="columns-1 md:columns-3">
+        <h3>Who we are</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+          architecto blanditiis veritatis sed impedit!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default About;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,16 @@
-
 function Home() {
-
-return (
-    <>  
-    <h1>Code and Chill</h1>
-    
+  return (
+    <>
+      <div className="bg-dark-blue flex flex-col md:flex-row md:space-x-4">
+        <div className="flex items-center justify-center md:w-1/2">
+          <img src="../../3.png" alt="logo" />
+        </div>
+        <div className="flex items-center justify-center md:w-1/2">
+          <h1 className="text-3xl text-white">Welcomes You</h1>
+        </div>
+      </div>
     </>
-)
+  );
 }
 
 export default Home;
