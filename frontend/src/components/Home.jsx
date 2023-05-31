fb8ecc496e477f5296b557fc75e5a118bad38c4f
kevinpan47 <kpan8055@gmail.com>
2023-05-31 02:58:08 +0000

    renamed logo image, moved to public folder

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     <>
       <div className="bg-dark-blue flex flex-col md:flex-row md:space-x-4">
         <div className="flex items-center justify-center md:w-1/2">
-          <img src="../../3.png" alt="logo" />
+          <img src="logo.png" alt="logo" />
         </div>
         <div className="flex items-center justify-center md:w-1/2">
           <h1 className="text-3xl text-white">Welcomes You</h1>
