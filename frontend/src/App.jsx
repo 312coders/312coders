e4b15a44848b7138402a36f77caa878f2c826b99
MJaurigue <jaurigue.michael@gmail.com>
2023-05-31 18:58:25 -0500

    added a footer to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/about" element={<About />} />
         </Routes>
       </Router>
+      <Footer />
     </>
   );
 }
