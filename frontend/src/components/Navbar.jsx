56fa29a485bde354a59cb3c00fdd26e50be4a154
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-06-11 16:19:17 -0500

    working on hambuger menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,51 +2,85 @@
 //import "./Navbar.css";
 import { Link } from "react-router-dom";
 import Hamburger from "./Hamburger";
+import "./Navbar.css";
+import { useState, useEffect } from "react";
 
 function Navbar() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
+  const closeMobileMenu = () => setHamburgerOpen(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setHamburgerOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", showButton);
+
+    return () => {
+      window.removeEventListener("resize", showButton);
+    };
+  }, []);
+
   return (
     <>
-      <div className="navbar bg-dark-blue text-white pb-3  max-w-4xl m-auto">
-        <nav>
-          <ul className="flex justify-center md:justify-end md:text-xl max-w-4xl mx-auto px-7 items-center">
-            <li className="mr-10 mt-5">
-              <Link
-                to="/"
-                className="hover:text-red-500 transition duration-500"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="mr-10 mt-5">
-              <Link
-                to="/about"
-                className="hover:text-red-500 transition duration-500"
-              >
-                About
-              </Link>
-            </li>
-            <li className="mr-10 mt-5">
-              <Link
-                to="/community"
-                className="hover:text-red-500 transition duration-500"
-              >
-                Community
-              </Link>
-            </li>
-            <li className="mr-10 mt-5">
-              <Link
-                to="/contact"
-                className="hover:text-red-500 transition duration-500"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <div className="Hamburger">
+      <nav>
+        <div className="navbar bg-dark-blue text-white pb-3  max-w-4xl m-auto">
+          <div>
+            <ul className="flex justify-center md:justify-end md:text-xl max-w-4xl mx-auto px-7 items-center ">
+              <li className="mr-10 mt-5">
+                <Link
+                  to="/"
+                  className="hover:text-red-500 transition duration-500"
+                  onClick={closeMobileMenu}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="mr-10 mt-5">
+                <Link
+                  to="/about"
+                  className="hover:text-red-500 transition duration-500"
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="mr-10 mt-5">
+                <Link
+                  to="/community"
+                  className="hover:text-red-500 transition duration-500"
+                  onClick={closeMobileMenu}
+                >
+                  Community
+                </Link>
+              </li>
+              <li className="mr-10 mt-5">
+                <Link
+                  to="/contact"
+                  className="hover:text-red-500 transition duration-500"
+                  onClick={closeMobileMenu}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="Hamburger" onClick={toggleHamburger}>
             <Hamburger />
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </>
   );
 }
