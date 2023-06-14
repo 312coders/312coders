afdae1b03d31ec87f89be33b005240d48cff47a1
MJaurigue <jaurigue.michael@gmail.com>
2023-06-14 18:04:00 -0500

    Discord Section: Edited picture and created a nice paragraph explaining a bit about the discord community

--- frontend/src/components/Discord.jsx
+++ frontend/src/components/Discord.jsx
@@ -1,13 +1,12 @@
 function Discord() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-center py-8">Discord</h1>
       <div className="bg-light-blue py-10 px-8 max-w-4xl m-auto">
-        <h3 className="text-center font-semibold text-3xl pb-5">Community</h3>
+        <h3 className="text-center font-semibold text-3xl pb-5">Discord</h3>
         <div className="column-1 md:columns-2 p-4">
-          <img src="/skyline.jpg" className="block object-scale-down rounded" alt="Skyline" />
+          <img src="/discord.jpg" className="block object-scale-down rounded" alt="Skyline" />
           <p className="text-center m-3 p-5">
-            Come join our Discord community!
+          Join our vibrant Discord community for software developers! Collaborate on projects, engage in code reviews, participate in hackathons, and discuss the latest tech trends. We foster a respectful and inclusive environment where knowledge sharing and networking thrive. Expand your skills, connect with like-minded individuals, and be part of our growing community. Join us today!
           </p>
           <div className="text-center">
             <a href="https://discord.com/invite/YOUR_INVITE_LINK" target="_blank" rel="noopener noreferrer">
