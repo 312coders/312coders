a445e4332a853821677823ef8fbaba28e5d793ad
MJaurigue <jaurigue.michael@gmail.com>
2023-06-14 17:39:02 -0500

    Discord Section: Creation

---
+++ frontend/src/components/Discord.jsx
@@ -0,0 +1,18 @@
+function Discord() {
+    return (
+      <>
+        <div className="bg-light-blue py-10 px-8 max-w-4xl m-auto">
+          <h3 className="text-center font-semibold text-3xl pb-5">Community</h3>
+          <div className="column-1 md:columns-2 p-4">
+            <img src="/skyline.jpg" className="block object-scale-down rounded" />
+            <p className="text-center m-3 p-5">
+             Come join our discord community! 
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Discord;
+  
