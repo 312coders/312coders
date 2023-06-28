2d3ee3d98cd48cbb940bddef2038c50fcc47f025
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-06-28 14:46:54 -0500

    added h2 and styled/made responsive

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,12 +7,13 @@ function Home() {
   return (
     <>
       <div className="background-img flex flex-col md:flex-row md:space-x-3 py-10 border-4 border-t-red-500 max-w-4xl m-auto">
-        <div className="flex items-center justify-center md:w-1/2">
+        <div className="intro items-center justify-center md:w-1/2">
           <h1 className="welcome text-6xl font-extrabold text-center">
             Code,
             <br />
             <span className="margin">312</span> Style
           </h1>
+          <h2 className="tag">Developing in the Chicagoland</h2>
         </div>
         <div className="flex items-center justify-center md:w-1/2">
           <img src="logo.png" alt="logo" />
