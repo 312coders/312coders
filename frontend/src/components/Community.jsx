536b24813c7c29c44526d5a80dfa0731218d25c5
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-07-19 12:52:44 -0500

    community section to be links

--- frontend/src/components/Community.jsx
+++ frontend/src/components/Community.jsx
@@ -2,15 +2,25 @@ function Community() {
   return (
     <>
       <div className="bg-light-blue py-10 px-8 max-w-4xl m-auto" id="community">
-        <h3 className="text-center font-semibold text-3xl pb-5">Community</h3>
         <div className="column-1 md:columns-2 p-4">
           <img src="/skyline.jpg" className="block object-scale-down rounded" />
-          <p className="text-center m-3 p-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-            quia deserunt tenetur excepturi beatae architecto ex animi ab
-            voluptate odio facilis, voluptatem aperiam, nam ullam nobis at,
-            itaque blanditiis quam?
-          </p>
+          <div className="text-center m-3 p-5 flex">
+            <article>
+              <h4>Our Backgroud</h4>
+              <a>A little more about us</a>
+              <p>Diverse Backgrounds</p>
+            </article>
+            <article>
+              <h4>Activity</h4>
+              <a>Our Blog</a>
+              <p>What we have been up to</p>
+            </article>
+            <article>
+              <h4>Community</h4>
+              <a>Get involved</a>
+              <p>Hang out with us</p>
+            </article>
+          </div>
         </div>
       </div>
     </>
