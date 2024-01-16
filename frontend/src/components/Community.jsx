1274d7e2496d6ef3c9effea3cd86917e7220ef58
Maaya <yamauchimaaya@gmail.com>
2024-01-16 15:46:12 -0600

    removed events from home

--- frontend/src/components/Community.jsx
+++ frontend/src/components/Community.jsx
@@ -10,7 +10,6 @@ function Community() {
           Become part of a vibrant community of passionate tech enthusiasts! Surrounding yourself with individuals who inspire and uplift can significantly contribute to personal and professional development. If you're unsure about where to start but desire to connect with exceptional individuals in the tech industry, keep an eye out for future events, insightful blog posts, and engaging content!
           </p>
         </div>
-        <h3 className="text-center font-semibold text-2xl p-5">Events</h3>
       </div>
     </>
   );
