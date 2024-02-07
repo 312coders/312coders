5cb6457dc59458006cbb6654ca944025b9112203
kevinpan47 <kpan8055@gmail.com>
2024-02-07 20:00:48 +0000

    add blog app w/summernote wysiwyg editor

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
+    'blog'
 ]
 
 MIDDLEWARE = [
--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -4,7 +4,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DEV_DB'),
+        'NAME': os.getenv('POSTGRES_PROD_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
